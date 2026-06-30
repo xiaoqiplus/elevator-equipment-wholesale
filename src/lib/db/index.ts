28745cf62ac80c84639f8e06fab5b3d63cd161ba
xiaoqiplus <2323641504@qq.com>
2026-06-30 18:49:37 +0800

    cleanup: remove vercel/nextauth/upstash/auth/b2b deps

--- src/lib/db/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./products";
-export * from "./quotation";
-export * from "./user";
--- src/lib/email/resend.ts
+++
@@ -1,5 +0,0 @@
-import { Resend } from "resend";
-
-const resendApiKey = process.env.RESEND_API_KEY;
-
-export const resend = resendApiKey ? new Resend(resendApiKey) : null;
